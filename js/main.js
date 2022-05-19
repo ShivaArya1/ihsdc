e7ff545dbc3b526fd5abef2857fb3e1c7171a4ef
suzayn <suzayn.ash55@gmail.com>
2022-05-19 20:19:23 +0545

    first commit

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function setActive() {
-    aObj = document.getElementById('ihsdc-navbarNav').getElementsByTagName('a');
-    for(i=0;i<aObj.length;i++) { 
-      if(document.location.href.indexOf(aObj[i].href)>=0) {
-        aObj[i].className='ihsdc-active';
-      }
-    }
-  }
